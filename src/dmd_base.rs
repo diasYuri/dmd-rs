ee9055e3dde37c2e8c586437760809077accc233
Yuri Dias <yuri.dazevedo@gmail.com>
2023-04-13 20:43:44 -0300

    adding DMDOperator

--- src/dmd_base.rs
+++ src/dmd_base.rs
@@ -1,5 +1,5 @@
 use ndarray::{Array, Array1, Array2, ArrayView1, ArrayView2, Axis, Ix1, s};
-use ndarray_linalg::{c64, Eigh, LstSq, Scalar, Solver, SVDDC};
+use ndarray_linalg::{c64, Eigh, LstSq, Scalar, Solver, SVDDC, SVD, UPLO};
 use std::collections::HashMap;
 use std::f64::consts::PI;
 use std::fs::File;
@@ -110,3 +110,74 @@ impl DMDBase {
         self.eigs.mapv(|eig| eig.re / self.original_time["dt"])
     }
 }
+
+
+pub struct DMDOperator {
+    svd_rank: usize,
+    exact: bool,
+    modes: Option<Array2<Complex64>>,
+    eigenvectors: Option<Array2<Complex64>>,
+    eigenvalues: Option<Array1<Complex64>>,
+}
+
+impl DMDOperator {
+    pub fn new(svd_rank: usize, exact: bool) -> Self {
+        DMDOperator {
+            svd_rank,
+            exact,
+            modes: None,
+            eigenvectors: None,
+            eigenvalues: None,
+        }
+    }
+
+    pub fn fit(&mut self, x: Array2<Complex64>, y: Array2<Complex64>) {
+        let svd_rank = if self.svd_rank == 0 {
+            std::cmp::min(x.dim().0, x.dim().1)
+        } else {
+            self.svd_rank
+        };
+
+        let svd = x.svd(true, true).unwrap();
+        let u = svd.0.unwrap();
+        let s = svd.1;
+        let vh = svd.2.unwrap();
+
+        let u_truncated = u.slice(s![.., 0..svd_rank]).to_owned();
+        let s_truncated = s.slice(s![0..svd_rank]).to_owned();
+        let vh_truncated = vh.slice(s![0..svd_rank, ..]).to_owned();
+
+        let s_inv = Array1::from_iter(s_truncated.iter().map(|&x| 1.0 / x)).diag();
+        let a_tilde = u_truncated
+            .t()
+            .dot(&y)
+            .dot(&vh_truncated)
+            .dot(&s_inv);
+
+        let eig_decomposition = a_tilde.eigh(UPLO::Upper).unwrap();
+        let eigenvectors = eig_decomposition.0;
+        let eigenvalues = eig_decomposition.1;
+
+        self.eigenvalues = Some(eigenvalues);
+        self.eigenvectors = Some(eigenvectors.clone());
+
+        if self.exact {
+            self.modes = Some(x.dot(&y).dot(&vh).dot(&s_inv).dot(&eigenvectors));
+        } else {
+            self.modes = Some(u.dot(&eigenvectors));
+        }
+    }
+
+    pub fn modes(&self) -> &Option<Array2<Complex64>> {
+        &self.modes
+    }
+
+    pub fn eigenvectors(&self) -> &Option<Array2<Complex64>> {
+        &self.eigenvectors
+    }
+
+    pub fn eigenvalues(&self) -> &Option<Array1<Complex64>> {
+        &self.eigenvalues
+    }
+}
+
